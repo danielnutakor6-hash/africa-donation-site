be56788d074b831af481b20cb19bd1e09c8e3c08
danielnutakor6-hash <danielnutakor6@gmail.com>
2025-10-16 17:14:42 +0000

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('donationForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Thank you! Redirecting to secure payment... (Integrate Stripe here)');
-    // In real: Stripe.redirectToCheckout({ sessionId: 'your-session' });
-});
